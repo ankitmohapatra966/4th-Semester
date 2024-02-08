0f8c2c2bc0ad38a2048ba8d291d16f591d831e43
ankitmohapatra966 <126703297+ankitmohapatra966@users.noreply.github.com>
2024-02-08 19:17:34 +0530

    Update FractionalKnapsack.cpp

--- DAA/FractionalKnapsack.cpp
+++ DAA/FractionalKnapsack.cpp
@@ -1,52 +1,53 @@
-#include <bits/stdc++.h> 
-using namespace std; 
-struct Item { 
-    int value, weight; 
-  
-    Item(int value, int weight) 
-        : value(value), weight(weight) 
-    { 
-    } 
-}; 
-bool cmp(struct Item a, struct Item b) 
-{ 
-    double r1 = (double)a.value / a.weight; 
-    double r2 = (double)b.value / b.weight; 
-    return r1 > r2; 
-} 
-double fractionalKnapsack(struct Item arr[], 
-                          int N, int size) 
-{ 
-    sort(arr, arr + size, cmp); 
-    int curWeight = 0; 
-    double finalvalue = 0.0; 
-    for (int i = 0; i < size; i++) { 
-
-        if (curWeight + arr[i].weight <= N) { 
-            curWeight += arr[i].weight; 
-            finalvalue += arr[i].value; 
-        } 
-
-         else { 
-            int remain = N - curWeight; 
-            finalvalue += arr[i].value * ((double)remain / arr[i].weight); 
-  
-            break; 
-        } 
-    }  
-    return finalvalue; 
-}  
-int main() 
-{ 
-    int N = 60; 
-   
-    Item arr[] = { { 100, 10 }, 
-                   { 280, 40 }, 
-                   { 120, 20 }, 
-                   { 120, 24 } };
-                    int size = sizeof(arr) / sizeof(arr[0]); 
-   
-    cout << "Maximum profit earned = "
-         << fractionalKnapsack(arr, N, size); 
-    return 0; 
-} 
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+struct Item
+{
+	int weight;
+	int value;
+	double ratio;
+};
+bool compare(Item a,Item b)
+{
+	return a.ratio>b.ratio;
+}
+double fractionalKnapsack(int W, vector<Item>& items)
+{
+	sort(items.begin(),items.end(),compare);
+	double totalvalue=0;
+	int currentweight=0;
+	for(int i=0;i<items.size();i++)
+	{
+		if(currentweight+items[i].weight<=W)
+		{
+			currentweight+=items[i].weight;
+			totalvalue+=items[i].value;
+		}
+		else
+		{
+			int remainingweight=W-currentweight;
+			totalvalue+=items[i].ratio*remainingweight;
+			break;
+		}
+	}
+	return totalvalue;
+}
+int main()
+{
+	int n,W;
+	cout<<"Enter the number of items:";
+	cin>>n;
+	cout<<"Enter the capacity of the knapsack:";
+	cin>>W;
+	vector<Item> items(n);
+	cout<<"Enter the weight and value of each item:"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cin>>items[i].weight>>items[i].value;
+		items[i].ratio=(double) items[i].value/items[i].weight;
+	}
+	double maxvalue=fractionalKnapsack(W,items);
+	cout<<"Maximum value obtained from the knapsack:"<<maxvalue<<endl;
+	return 0;
+}
